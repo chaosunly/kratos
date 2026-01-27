7ea997bf0206b1af1c0d3ce68f7103407125a16e
Sunly <sunly.chao@rexform.io>
2026-01-27 23:18:54 +0700

    fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -e
 
-# Set default values for environment variables
-export KRATOS_PUBLIC_URL="${KRATOS_PUBLIC_URL:-http://localhost:4433}"
-export KRATOS_ADMIN_URL="${KRATOS_ADMIN_URL:-http://localhost:4434}"
-export DEFAULT_RETURN_URL="${DEFAULT_RETURN_URL:-http://localhost:3000}"
-export ALLOWED_RETURN_URL="${ALLOWED_RETURN_URL:-http://localhost:3000}"
-export KRATOS_UI_URL="${KRATOS_UI_URL:-http://localhost:3000}"
-
-# Railway injects PORT dynamically
-export SERVE_PUBLIC_PORT="${PORT:-4433}"
-
 # Substitute environment variables in the config file
 envsubst < /etc/kratos/kratos.yml > /tmp/kratos.yml
 
