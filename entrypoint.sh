d76e985bab8cf8cad0ab8cce64cfd23f132ab9b5
Sunly <sunly.chao@rexform.io>
2026-01-29 15:36:08 +0700

    config Origins

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,12 @@ export SERVE_PUBLIC_PORT="${PORT:-8080}"
 : "${KRATOS_UI_URL:?missing}"
 : "${DEFAULT_RETURN_URL:?missing}"
 : "${ALLOWED_RETURN_URL:?missing}"
+: "${CORS_ALLOWED_ORIGIN:?missing}"
 : "${DSN:?missing}"
 : "${SECRETS_DEFAULT:?missing}"
 : "${SECRETS_COOKIE:?missing}"
 : "${COURIER_SMTP_CONNECTION_URI:?missing}"
 
-# Render config
 envsubst < /etc/kratos/kratos.yml > /tmp/kratos.yml
-
-# Run DB migrations
 kratos -c /tmp/kratos.yml migrate sql -e --yes
-
-# Start Kratos
-kratos -c /etc/kratos/kratos.yml serve --watch-courier
+kratos -c /tmp/kratos.yml serve --watch-courier
