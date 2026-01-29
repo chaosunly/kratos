1dabcf6504645de512d1c16cc9a218f924aad62e
Sunly <sunly.chao@rexform.io>
2026-01-29 15:23:54 +0700

    config Origins

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 export SERVE_PUBLIC_PORT="${PORT:-8080}"
 
-# Substitute environment variables in the config file
-envsubst < /etc/kratos/kratos.yml > /tmp/kratos.yml
-
 # Run DB migrations
 kratos -c /tmp/kratos.yml migrate sql -e --yes
 
