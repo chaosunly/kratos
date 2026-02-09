1d68686f57f08adc6195c904ef6b5e857eb562ff
Sunly <sunly.chao@rexform.io>
2026-02-09 22:56:27 +0700

    add env

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,13 @@ set -e
 : "${KRATOS_PUBLIC_URL:?missing}"
 : "${ALLOWED_RETURN_URL:?missing}"
 : "${CORS_ALLOWED_ORIGIN:?missing}"
+: "${COOKIE_DOMAIN:?missing}"
 : "${DSN:?missing}"
 : "${SECRETS_DEFAULT:?missing}"
 : "${SECRETS_COOKIE:?missing}"
 : "${COURIER_SMTP_CONNECTION_URI:?missing}"
+: "${COURIER_SMTP_FROM_ADDRESS:?missing}"
+: "${COURIER_SMTP_FROM_NAME:?missing}"
 
 # Generate Kratos config
 envsubst < /etc/kratos/kratos.yml > /tmp/kratos.yml
@@ -16,5 +19,5 @@ envsubst < /etc/kratos/kratos.yml > /tmp/kratos.yml
 # Run migrations
 kratos -c /tmp/kratos.yml migrate sql -e --yes
 
-# Start Kratos (no public exposure - accessed via gateway only)
+# Start Kratos
 exec kratos -c /tmp/kratos.yml serve --watch-courier
