aa97eaa353fb22a9d51c50319e136a8ec26181e6
Sengly456 <senglyou84@gmail.com>
2026-02-26 14:58:56 +0700

    Use working SimpleLogin OIDC config with base64 mapper

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Ensure these exist (Railway sets them, this just makes it explicit)
-: "${KRATOS_PUBLIC_URL:?missing}"
+: "${ORY_KRATOS_PUBLIC_URL:?missing}"
 : "${KRATOS_UI_URL:?missing}"
 : "${DEFAULT_RETURN_URL:?missing}"
 : "${ALLOWED_RETURN_URL:?missing}"
